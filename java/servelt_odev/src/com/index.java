4d84d6455f8958b85b4e9eb23ac576fdff9a6ae4
erozkos <erozkos@hotmail.com>
2013-03-26 00:21:06 +0200

    servlet-thread

---
+++ java/servelt_odev/src/com/index.java
@@ -0,0 +1,121 @@
+package com;
+ 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class index
+ */
+@WebServlet("/index")
+public class index extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public index() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+
+
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html><html><head>" +
+				
+				"</head><body>");
+
+/* Ogrenci sýnýfý */
+		Ogrenci ogr = new Ogrenci(11060649, "Ersin", "ÖZKÖSE", 1989, 21, 05, "Bil-Muh", "Samsun");
+		out.println("<div> Ogrenci");
+		out.println("<ul>");
+		out.println(
+				"<li>Ogrenci No : " + ogr.getOgrNo() + "</li>" +
+				"<li>Ad : " + ogr.getAd() + "</li>" +
+				"<li>Soyad : " + ogr.getSoyad() + "</li>" +
+				"<li>Sýnýf : " + ogr.getSinif() + "</li>" +
+				"<li>Bolum: " + ogr.getBolum() + "</li>" +
+				"<li>Memleket : " + ogr.getMemleket() + "</li></ul>"
+			   );
+		out.println(
+				ogr.DerseGir() + "</br>" +
+				ogr.KantineGit()
+			   );
+		out.println("</div>");
+
+/* Memur Sýnýfý  */
+		Memur mmr = new Memur(123123123, "Salih", "ÇAKIR", 1987, 271712123, "Ogrenci isleri");
+		out.println("<div> Memur");
+		out.println("<ul>");
+		out.println(
+				"<li> TC : " + mmr.getTcNo() + "</li>" +
+				"<li> Ad : " + mmr.getAd() + "</li>" +
+				"<li> Soyad : " + mmr.getSoyad() + "</li>" +
+				"<li> Dogum Tarihi : " + mmr.getDtarihi() + "</li>" +
+				"<li> Sicil No : " + mmr.getSicilNo() + "</li>" +
+				"<li> Birim : " + mmr.getBirim() + "</li></ul>"
+			   );
+
+		out.println(
+				mmr.EvrakGetir() + "</br>" +
+				mmr.EvrakGotur()
+			   );
+		out.println("</div>");
+
+/* Ogretmen sýnýfý */
+		Ogretmen ort = new Ogretmen(321321321, "Sadi", "CENGÝZ", 1967, 112112123, "Matematik");
+		out.println("<div> Ogretmen");
+		out.println("<ul>");
+		out.println(
+				"<li> TC : " + ort.getTcNo() + "</li>" +
+				"<li> Ad : " + ort.getAd() + "</li>" +
+				"<li> Soyad : " + ort.getSoyad() + "</li>" +
+				"<li> Dogum Tarihi : " + ort.getDtarihi() + "</li>" +
+				"<li> Sicil No : " + ort.getSicilNo() + "</li>" +
+				"<li> Bolum : " + ort.getBolum() + "</li></ul>"
+			   );
+
+		out.println(
+				ort.SinavYap() + "</br>" +
+				ort.DerseGir()
+			   );
+		out.println("</div>");
+
+/* Ogretim uyesi sýnýfý  */
+		OgretimUyesi ogr_uye = new OgretimUyesi(900123123, "MÜNÝR", "KOÇ", 1977, 929292, "Makina", "Yrd.Doç.Dr");
+		out.println("<div> Ogretim Uyesi");
+		out.println("<ul>");
+		out.println(
+				"<li> TC : " + ogr_uye.getTcNo() + "</li>" +
+				"<li> Ad : " + ogr_uye.getAd() + "</li>" +
+				"<li> Soyad : " + ogr_uye.getSoyad() + "</li>" +
+				"<li> Dogum Tarihi : " + ogr_uye.getDtarihi() + "</li>" +
+				"<li> Sicil No : " + ogr_uye.getSicilNo() + "</li>" +
+				"<li> Bolum : " + ogr_uye.getBolum() + "</li>" +
+				"<li> Unvan : " + ogr_uye.getUnvan() + "</li></ul>"
+			   );
+		out.println("</div>");
+		out.println("</html></body>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ java/threaduy/src/Uygulama.java
@@ -0,0 +1,13 @@
+public class Uygulama
+{
+   public static void main(String args[])
+   {
+    Islem i1 = new Islem("Ersin ÖZKÖSE", 4); 
+    Islem i2 = new Islem("CENG",1); 
+    Islem i3 = new Islem("OMU", 2); 
+
+    i1.start();
+    i2.start();
+    i3.start();
+   }
+}
