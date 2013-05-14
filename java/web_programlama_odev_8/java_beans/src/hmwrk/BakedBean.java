139c0f41cf179a5f66775fa6cb54cd919d495486
erozkos <erozkos@hotmail.com>
2013-05-14 23:23:27 +0300

    odev8

---
+++ java/web_programlama_odev_8/java_beans/src/hmwrk/BakedBean.java
@@ -0,0 +1,24 @@
+package hmwrk;
+
+public class BakedBean {
+	
+	private String name = "ersin";
+	private String surname = "ÖZKÖSE";
+	
+	public String getname() {
+		return(name);
+	}
+	
+	public void setname(String newname) {
+		name = newname;
+	}
+	
+	public String getsurname() {
+		return(surname);
+	}
+	
+	public void setsurname(String n_surname) {
+		surname = n_surname;
+	}
+
+}
